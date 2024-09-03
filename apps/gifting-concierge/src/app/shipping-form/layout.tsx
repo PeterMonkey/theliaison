300793356ea1eafad84f7aa101ec4bfc00d990ca
Pedro Fernandez <pefernandez1408@gmail.com>
2024-09-03 12:12:36 -0400

    add footer in map page

--- apps/gifting-concierge/src/app/shipping-form/layout.tsx
+++ apps/gifting-concierge/src/app/shipping-form/layout.tsx
@@ -1,10 +1,12 @@
 import { Header } from "~/components/header"
+import { Footer } from "../components/footer"
 
 export default function Layout({children}:{children: React.ReactNode}) {
     return(
         <div className="flex min-h-dvh flex-col bg-background">
             <Header/>
             {children}
+            <Footer/>
         </div>
     )
 }
--- apps/gifting-concierge/src/app/shipping-form/page.tsx
+++ apps/gifting-concierge/src/app/shipping-form/page.tsx
@@ -2,7 +2,7 @@ import StoreLocator from "./StoreLocator"
 
 export default function AddressInfo() {
     return(
-        <div className="mt-4">
+        <div className="mt-4 mb-32">
             <StoreLocator/>
         </div>
     )
